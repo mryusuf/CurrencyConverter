c9756d3d8c6b7d1c271ec519dfbdb604f52274da
Muhamad Yusuf Indra <muhamadyusufindra@gmail.com>
2020-02-06 10:17:07 +0700

    Initial Commit

---
+++ Currency Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by Indra Permana on 06/02/20.
+//  Copyright © 2020 Yusuf Indra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
